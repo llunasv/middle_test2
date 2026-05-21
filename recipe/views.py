95e80c6daf3e161e3e0349c26ab08413b727e168
llunasv <vladaglov@gmail.com>
2026-05-21 13:23:13 +0300

    feat: add main view with 10 random recipes

--- recipe/views.py
+++ recipe/views.py
@@ -4,8 +4,11 @@ from .models import Recipe, Category
 def main(request):
     recipes = Recipe.objects.order_by('?')[:10]
     return render(request, 'main.html', {'recipes': recipes})
+    # Не збережений коміт / Відображення головної сторінки з 10 випадковими рецептами
+
 
 def category_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     recipes = Recipe.objects.filter(category=category)
     return render(request, 'category_detail.html', {'category': category, 'recipes': recipes})
+
